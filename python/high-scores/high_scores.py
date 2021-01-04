372c2b91b56bce7d614532f4dbb3b8b1639819c3
kamil <kamil.chruscinski1@gmail.com>
2021-01-04 09:47:14 +0100

    deleted redundant code/other solutions

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,27 +1,13 @@
-# def latest(scores):
-#     return scores[-1]
-
-
-# def personal_best(scores):
-#     scores.sort()
-#     return scores[-1]
-
-
-# def personal_top_three(scores):
-#     scores.sort()
-#     scores.reverse()
-#     return scores[:3]
-
 def latest(scores):
-    """ Return latest score """
     return scores[-1]
 
 
 def personal_best(scores):
-    """ Return the best score """
-    return max(scores)
+    scores.sort()
+    return scores[-1]
 
 
 def personal_top_three(scores):
-    """ Return three best scores """
-    return sorted(scores, reverse=True)[0:3]
+    scores.sort()
+    scores.reverse()
+    return scores[:3]
--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,27 +1,15 @@
-# def convert(number):
-#     sound = ''
-#     if number %  3 == 0:
-#         sound += 'Pling'
-    
-#     if number %  5 == 0:
-#         sound += 'Plang'
-    
-#     if number %  7 == 0:
-#         sound += 'Plong'
-
-#     if sound == '':
-#         return str(number)
-    
-#     return sound
 def convert(number):
-    result = ''
-    lookup = {
-        3: 'Pling',
-        5: 'Plang',
-        7: 'Plong'
-    }
-    for n in lookup:
-        if not number % n:
-            result += lookup[n]
+    sound = ''
+    if number %  3 == 0:
+        sound += 'Pling'
 
-    return result if result else str(number)
+    if number %  5 == 0:
+        sound += 'Plang'
+
+    if number %  7 == 0:
+        sound += 'Plong'
+
+    if sound == '':
+        return str(number)
+
+    return sound
