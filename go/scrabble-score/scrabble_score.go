725612e94e598843e0b5b06ffd87e444b3ab43c4
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-01-18 21:12:50 +0100

    added solution for scrabble_score in Go track

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,7 +1,42 @@
+// Package scrabble computes words to the Scrabble score
 package scrabble
 
-// map i loop sprawdzic
+import "strings"
 
-func scrabbleScore() {
-    //todo
+// Score computes the Scrabble score for given word
+func Score(word string) int {
+    word = strings.ToLower(word)
+    letters := map[rune]int {
+        'a':1,
+        'e':1,
+        'i':1,
+        'o':1,
+        'u':1,
+        'l':1,
+        'n':1,
+        'r':1,
+        's':1,
+        't':1,
+        'd':2,
+        'g':2,
+        'b':3,
+        'c':3,
+        'm':3,
+        'p':3,
+        'f':4,
+        'h':4,
+        'v':4,
+        'w':4,
+        'y':4,
+        'k':5,
+        'j':8,
+        'x':8,
+        'q':10,
+        'z':10,
+    }
+    points := 0
+    for _, letter := range word {
+            points += letters[letter]
+    }
+    return points
 }
