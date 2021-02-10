ad3b70042ab50cdabefc8695e403e4b439e830bc
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-02-10 22:06:36 +0100

    completed the challenge

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -14,16 +14,26 @@ class Garden:
     }
 
     def __init__(self, diagram, students=students):
-        self.diagram = diagram
+        self.diagram = diagram.split("\n")
         self.students = students
         students.sort()
 
 
     def plants(self, student):
-        i = self.students.index(student)
-#         print(i)
-#
-#
-#
-# t = Garden("VVCG\nVVRC")
-# print(t.plants("Bob"))
+        i = (self.students.index(student) + 1) * 2
+        plant_list = []
+
+        pair1 = self.diagram[0][i-2:i]
+        pair2 = self.diagram[1][i-2:i]
+
+        for s in pair1:
+            plant_list.append(self.seeds[s])
+
+        for c in pair2:
+            plant_list.append(self.seeds[c])
+
+        return plant_list
+
+
+# t = Garden("VVCCGG\nVVCCGG")
+# print(t.plants("Charlie"))
