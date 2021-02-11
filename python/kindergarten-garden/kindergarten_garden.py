e41b90f746ef120efb769057b64ab055f4a17479
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-02-11 12:52:44 +0100

    commented the code

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,11 +1,13 @@
 class Garden:
 
+    # basic student group
     students = [
         'Alice', 'Bob', 'Charlie', 'David',
         'Eve', 'Fred', 'Ginny', 'Harriet',
         'Ileana', 'Joseph', 'Kincaid', 'Larry'
     ]
 
+    # seed names reference
     seeds = {
         "G": "Grass",
         "C": "Clover",
@@ -13,16 +15,20 @@ class Garden:
         "V": "Violets"
     }
 
+    # init func, setting students arg as a default student list
     def __init__(self, diagram, students=students):
-        self.diagram = diagram.split("\n")
+        self.diagram = diagram.split("\n") # split diagram by 2 rows
         self.students = students
+        # alphabetically sorting students in case of getting different list
         students.sort()
 
 
     def plants(self, student):
+        # get student index from the list and multiply it so it corresponds to seeds
         i = (self.students.index(student) + 1) * 2
         plant_list = []
 
+        # get plants from diagram using i variable
         plants = self.diagram[0][i-2:i] + self.diagram[1][i-2:i]
         for s in plants:
             plant_list.append(self.seeds[s])
