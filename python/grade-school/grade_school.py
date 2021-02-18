d8a42f82fae67ea8879c3bbf4316b8c3c3923f68
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-02-18 11:22:52 +0100

    completed the project

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,12 +1,22 @@
 class School:
+
     def __init__(self):
-        pass
+        self.students = dict()
 
     def add_student(self, name, grade):
-        pass
+        self.name = name
+        self.student_grade = grade
+        self.students[name] = grade
 
     def roster(self):
-        pass
+        sorted_by_grade = {k: v for k, v in sorted(sorted(self.students.items()), key=lambda item: item[1])}
+        return list(sorted_by_grade)
 
     def grade(self, grade_number):
-        pass
+        output = []
+
+        for student in self.students.items():
+            if student[1] == grade_number:
+                output.append(student[0])
+
+        return sorted(output)
