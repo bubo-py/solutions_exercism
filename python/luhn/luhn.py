13a2ad709f5b90cac4ec3d8604a7f71a4ef836e3
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-02-25 08:20:17 +0100

    commented the code

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -3,15 +3,19 @@ class Luhn:
         self.card_num = card_num.replace(' ', '')
 
     def valid(self):
+        # simple validation if given input is possible to process
         if self.card_num.isdigit() and len(self.card_num) > 1:
+            # looping through every 2 number, starting from second last one
             to_check = [int(n) for n in self.card_num[-2::-2]]
             new_numbers = []
+
             for x in to_check:
                 x = x * 2
                 if x > 9:
                     x = x - 9
                 new_numbers.append(x)
 
+            # sum of the doubled numbers and the rest
             the_sum = sum(new_numbers) + sum([int(n) for n in self.card_num[::-2]])
         else:
             return False
