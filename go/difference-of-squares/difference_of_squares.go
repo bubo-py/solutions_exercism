102ac7c93406e57b58b759c58404139d8f4c1ff5
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-03-01 14:17:02 +0100

    gofmt'ed the code

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -5,23 +5,23 @@ import "math"
 
 // SquareOfSum calculates the square of the sum
 func SquareOfSum(n int) int {
-    var sum int = 0
-    for x := 0; x <= n; x++ {
-        sum += x
-    }
-    return int(math.Pow(float64(sum), 2))
+	var sum int = 0
+	for x := 0; x <= n; x++ {
+		sum += x
+	}
+	return int(math.Pow(float64(sum), 2))
 }
 
 // SumOfSquares calculates the sum of the squares
 func SumOfSquares(n int) int {
-    var squaredSum float64 = 0
-    for x := 0; x <= n; x++ {
-        squaredSum += math.Pow(float64(x), 2)
-    }
-    return int(squaredSum)
+	var squaredSum float64 = 0
+	for x := 0; x <= n; x++ {
+		squaredSum += math.Pow(float64(x), 2)
+	}
+	return int(squaredSum)
 }
 
 // Difference calculates the difference between squares, using above functions
 func Difference(n int) int {
-    return SquareOfSum(n) - SumOfSquares(n)
+	return SquareOfSum(n) - SumOfSquares(n)
 }
