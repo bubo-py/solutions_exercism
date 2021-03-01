6bb4a918b5effb8a6836ba2c6be2ac44c78041b4
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-03-01 14:07:38 +0100

    completed the challange

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,1 +1,23 @@
 package diffsquares
+
+import "math"
+
+func SquareOfSum(n int) int {
+    var sum int = 0
+    for x := 0; x <= n; x++ {
+        sum += x
+    }
+    return int(math.Pow(float64(sum), 2))
+}
+
+func SumOfSquares(n int) int {
+    var squaredSum float64 = 0
+    for x := 0; x <= n; x++ {
+        squaredSum += math.Pow(float64(x), 2)
+    }
+    return int(squaredSum)
+}
+
+func Difference(n int) int {
+    return SquareOfSum(n) - SumOfSquares(n)
+}
