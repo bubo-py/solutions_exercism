55b98ba1d5760a2c1c9567326e6ab1efca8ff1f2
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-03-01 11:53:23 +0100

    completed triangle challenge

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,33 +1,49 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package triangle should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package triangle determines a type of triangle
 package triangle
 
+import "math"
 
-// Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+// Kind is a type of triangle
+type Kind string
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	NaT = "not a triangle"
+	Equ = "equilateral"
+	Iso = "isosceles"
+	Sca = "scalene"
 )
 
-// KindFromSides should have a comment documenting it.
+// KindFromSides determines a type of triangle
 func KindFromSides(a, b, c float64) Kind {
 	var k Kind
+	if isValidTriangle(a, b, c) {
+		switch {
+		case a == b && b == c:
+			k = Equ
+		case a == b || a == c || b == c:
+			k = Iso
+		case a != b && a != c && b != c:
+			k = Sca
+		}
+	} else {
+		k = NaT
+	}
 	return k
 }
 
-func isTriangle (a, b, c int) bool {
-    if a > 0 && b > 0 && c > 0 {
-        if a <= b + c && b <= c + a && c <= a + b {
-            return true
-        }
-    }
-    return false
+// check if given sides can make a valid triangle
+func isValidTriangle(a, b, c float64) bool {
+	if isValidNumber(a) && isValidNumber(b) && isValidNumber(c) {
+		if a > 0 && b > 0 && c > 0 {
+			if a <= b+c && b <= c+a && c <= a+b {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// check if number is valid in all infinity cases
+func isValidNumber(number float64) bool {
+	return !math.IsInf(number, 1) && !math.IsInf(number, -1) && !math.IsNaN(number)
 }
