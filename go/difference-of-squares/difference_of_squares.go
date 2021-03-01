e8991c74d112aad63206f3b35ce96d0c5714b286
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-03-01 14:11:29 +0100

    commented the solution

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,7 +1,9 @@
+// Package diffsquares calculates the difference between definite squares
 package diffsquares
 
 import "math"
 
+// SquareOfSum calculates the square of the sum
 func SquareOfSum(n int) int {
     var sum int = 0
     for x := 0; x <= n; x++ {
@@ -10,6 +12,7 @@ func SquareOfSum(n int) int {
     return int(math.Pow(float64(sum), 2))
 }
 
+// SumOfSquares calculates the sum of the squares
 func SumOfSquares(n int) int {
     var squaredSum float64 = 0
     for x := 0; x <= n; x++ {
@@ -18,6 +21,7 @@ func SumOfSquares(n int) int {
     return int(squaredSum)
 }
 
+// Difference calculates the difference between squares, using above functions
 func Difference(n int) int {
     return SquareOfSum(n) - SumOfSquares(n)
 }
