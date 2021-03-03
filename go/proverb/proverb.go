a52909fdf421b047e26b290463f3b124f2e18642
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-03-03 20:57:38 +0100

    completed proverb challenge

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -1,15 +1,23 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package proverb generates bunch of proverb lines with given words
 package proverb
 
-// Proverb should have a comment documenting it.
+import "fmt"
+
+// Proverb generates bunch of proverb lines with given words
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+	var result []string
+	var end string
+
+	if len(rhyme) != 0 && len(rhyme) != 1 {
+		for i := 0; i < len(rhyme)-1; i++ {
+			line := fmt.Sprintf("For want of a %v the %v was lost.", rhyme[i], rhyme[i+1])
+			result = append(result, line)
+		}
+	}
+
+	if len(rhyme) != 0 {
+		end = fmt.Sprintf("And all for the want of a %v.", rhyme[0])
+		result = append(result, end)
+	}
+	return result
 }
