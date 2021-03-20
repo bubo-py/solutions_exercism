c4c476eba919b867597f54f69bceee74c873e081
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-03-20 15:10:33 +0100

    completed the challenge

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,10 @@
 def is_armstrong_number(number):
-    pass
+
+    pow = len(str(number))
+    sum = 0
+
+    for n in str(number):
+        n = int(n)
+        sum += n ** pow
+
+    return True if sum == number else False
