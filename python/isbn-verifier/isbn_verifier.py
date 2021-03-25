ab487dc89b11b8145660c6bbe97398138b6c51de
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-03-25 22:07:11 +0100

    commented and cleaned the code

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,18 +1,19 @@
 def is_valid(isbn):
-    isbn = isbn.replace("-", "")
+    isbn = isbn.replace("-", "") # remove any dashes
+    # initial values
     sum = 0
     m_number = 10
 
+    # validate the isbn length and remove 'X' character if exists
     if len(isbn) == 10 and isbn[-1] == "X":
         isbn = isbn.replace("X", "0")
         sum += 10
 
+    # validate the isbn and calculate its components
     if len(isbn) == 10 and isbn.isdigit():
-
         for n in isbn:
             sum += int(n) * m_number
             m_number -= 1
-
     else:
         return False
 
