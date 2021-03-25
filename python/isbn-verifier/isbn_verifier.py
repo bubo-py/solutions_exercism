26c0f51b7fb7e047a485fb76420947c918431595
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-03-25 21:57:51 +0100

    completed the challenge

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,21 @@
 def is_valid(isbn):
-    pass
+    isbn = isbn.replace("-", "")
+    sum = 0
+    m_number = 10
+
+    if len(isbn) == 10 and isbn[-1] == "X":
+        isbn = isbn.replace("X", "0")
+        sum += 10
+
+    if len(isbn) == 10 and isbn.isdigit():
+
+        for n in isbn:
+            sum += int(n) * m_number
+            m_number -= 1
+
+    else:
+        return False
+
+    return True if sum % 11 == 0 else False
+
+print(is_valid("359821507X"))
