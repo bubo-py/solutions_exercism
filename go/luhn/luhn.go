db0e483f76ef4d8bce985933e7e1458711d59d6f
Kamil Chruściński <kamil.chruscinski1@gmail.com>
2021-03-28 21:27:45 +0200

    updated solution (still incomplete)

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,1 +1,25 @@
 package luhn
+
+import (
+    "strings"
+    "strconv"
+)
+
+func Valid (str string) bool {
+    sum := 0
+    n := 0
+    str = strings.Replace(str, " ", "", -1)
+
+    if _, err := strconv.Atoi(str); err == nil && len([]rune(str)) > 1 {
+        for range str {
+            toAdd, _ := strconv.Atoi(string(str[n]))
+            sum += toAdd
+            n += 2
+        }
+    }
+
+    // if sum % 10 == 0 {
+    //     return true
+    // }
+    return false
+}
